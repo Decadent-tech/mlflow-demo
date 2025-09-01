f3404e027480dddda7c8fcade20021e5f7dd9d04
Decadent-tech <debosmitac8@gmail.com>
2025-09-01 11:32:21 +0530

    Save report to HTML

--- drift_check.py
+++ drift_check.py
@@ -1,25 +1,32 @@
-# drift_check.py
 import pandas as pd
+import evidently
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from evidently import Report
-from evidently.metric_preset  import DataDriftPreset
-
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset
 
 # Load reference & current data
 X, y = load_iris(return_X_y=True, as_frame=True)
 X_ref, X_cur, _, _ = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Drift report
+print(evidently.__version__)
+# Create and run drift report
 report = Report(metrics=[DataDriftPreset()])
 report.run(reference_data=X_ref, current_data=X_cur)
-# Save HTML report
-report.save_html("drift_report.html")
+
+# Save report to HTML
+report.save("drift_report.html")
+
+# Extract drift detection result
 summary = report.as_dict()
 print(summary)
 
-if summary["metrics"][0]["result"]["dataset_drift"]:
+try:
+    drift_detected = summary["metrics"][0]["result"]["dataset_drift"]
+except KeyError:
+    drift_detected = summary["metrics"][0]["result"].get("drift_detected", False)
+
+if drift_detected:
     print("⚠️ Data drift detected!")
-    exit(1)  # Fail workflow if drift found
+    exit(1)
 else:
     print("✅ No data drift detected")
